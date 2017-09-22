27d4f5127f8153b26106e9291082f496fc2a57c3
Pablo Gomez <flipy34@gmail.com>
2017-09-22 07:22:02 -0700

    Forgot challenge API
    Forgot to get challenge API in baseactivity.

--- app/src/main/java/com/wag/challenge/activity/BaseActivity.java
+++ app/src/main/java/com/wag/challenge/activity/BaseActivity.java
@@ -1,9 +1,13 @@
 package com.wag.challenge.activity;
 
+import android.os.Bundle;
 import android.support.annotation.IdRes;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+
+import com.wag.challenge.interfaces.ChallengeApi;
 
 /**
  * Created by PGomez on 9/21/2017.
@@ -11,6 +15,12 @@ import android.support.v7.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
 
+    protected ChallengeApi challengeApi;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     /**
      * Base method to adding fragments to any activity
